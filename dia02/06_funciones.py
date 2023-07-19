31d92875d4e2c34e3219c41406d18d4fd9b0de34
Paolo Soncco <paholo2054@gmail.com>
2023-07-18 21:37:15 -0500

    funciones

--- dia02/06_funciones.py
+++ dia02/06_funciones.py
@@ -1,6 +1,31 @@
 def miFuncion():
     print("Hola Mundo")
 
-miFuncion()
+# miFuncion()
 
 # Crear una funcion que sume dos numeros cualquiera
+
+def suma(a, b):
+    return a + b
+
+# print(suma(5, 4))
+
+
+# Recibir una lista de argumentos o parametros (*args)
+def suma2(*args):
+    return sum(args)
+
+# print(suma2(1, 2.5, 3, 4, 5, 6))
+
+
+# Recibir argumentos arbitrarios con clave-valor (**kwargs)
+def personas(**args):
+    print(args)
+
+personas(nombre="Juan", apellido="Perez", edad=25)
+
+# Definir un parametro por defecto
+def suma3(a=3, b=5):
+    return a + b
+
+print(suma3(6, 6))
