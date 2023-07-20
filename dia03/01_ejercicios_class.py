d1f08429b1acbd9712bd90d65df39794f2596252
Paolo Soncco <paholo2054@gmail.com>
2023-07-19 21:02:24 -0500

    ejercicios POO

--- dia03/01_ejercicios_class.py
+++ dia03/01_ejercicios_class.py
@@ -32,11 +32,35 @@ class Gato:
 gato = Gato("Michi", "Blanco")
 gato.estaDurmiendo = False
 gato.cambiarEstado(True)
-gato.estado()
+# gato.estado()
 
 """Crear un objeto Usuario que tenga los atributos id, nombre, email, password
 que tenga un metodo que me muestre sus datos ordenados en un diccionario
 y otro metodo que encripte la password del usuario"""
 
 import hashlib
+from pprint import pprint
 
+class Usuario:
+    def __init__(self, id, nombre, email, password):
+        self.id = id
+        self.nombre = nombre
+        self.correo = email
+        self.password = self.hashPassword(password)
+
+    def mostrarUsuario(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "correo": self.correo,
+            "password": self.password
+        }
+    
+    def hashPassword(self, password):
+        pwd_bytes = password.encode()
+        hash_pwd = hashlib.sha256(pwd_bytes).hexdigest()
+        return hash_pwd
+    
+usuario = Usuario(1, "Cesar Mayta", "cesar@gmail.com", "osito123")
+
+pprint(usuario.mostrarUsuario())
