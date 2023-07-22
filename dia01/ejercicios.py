05ca79f72be7f14a5ecb9d5a969f081389461e3e
Paolo Soncco <paholo2054@gmail.com>
2023-07-21 22:58:49 -0500

    primer contacto flask

--- dia01/ejercicios.py
+++
@@ -1,7 +0,0 @@
-# edad = input("¿Cuántos años tienes? ")
-# print(edad)
-
-# Recibir dos numero e imprimir la suma de estos dos
-primer_numero = float(input("Escribe un número: "))
-segundo_numero = float(input("Escribe otro número: "))
-print(primer_numero + segundo_numero)
