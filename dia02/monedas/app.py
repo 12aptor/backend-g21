cfae1a0801b6824f0a53ae1f035a18dcb52548d9
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-25 20:45:27 -0500

    tipo cambio

--- dia02/monedas/app.py
+++ dia02/monedas/app.py
@@ -1,11 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
-
+TITULO_PAGINA = "TIPO DE CAMBIO DE LA SBS"
 @app.route('/')
 def index():
-    titulo = "TIPO DE CAMBIO DE LA SBS"
-    return render_template('index.html',titulo=titulo)
+    
+    return render_template('index.html',titulo=TITULO_PAGINA)
+
+@app.route('/convertir',methods=['POST'])
+def convertir():
+    if request.method == "POST":
+        monto_soles = request.form['soles']
+        resultado =  round(float(monto_soles) / 3.588,2)
+        
+    return render_template('index.html',titulo=TITULO_PAGINA,monto=resultado)
+
 
 app.run(debug=True)
 
