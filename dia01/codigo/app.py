932be0aae3c6b3a5afd942f30f4efc41073552ee
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-25 19:34:32 -0500

    dia1 ejercicio

--- dia01/codigo/app.py
+++ dia01/codigo/app.py
@@ -24,6 +24,26 @@ def suma():
  a multiplicación debe multiplicar, si escribes otra operación distinta
  debe salir un mensaje que diga OPERACION INVALIDA
 """
+@app.route('/calculadora')
+def calculadora():
+    n1 = int(request.args.get('n1','0'))
+    n2 = int(request.args.get('n2','0'))
+    ope = request.args.get('ope','ninguno')
+    
+    if(ope == 'suma'):
+        resultado = n1 + n2
+    elif(ope == 'resta'):
+        resultado = n1 - n2
+    elif(ope == 'multiplicacion'):
+        resultado = n1 * n2
+    else:
+        resultado = None
+        
+    if resultado is not None:
+        return '<h1>resultado es {}</h1>'.format(resultado)
+    else:
+        return '<h1>OPERACIÓN INVALIDA</h1>'
+
 
 app.run(debug=True)
 
