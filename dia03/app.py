21db686a4fdfec4321a130244d19059a057d81d5
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-26 21:27:40 -0500

    dia3 templates

--- dia03/app.py
+++ dia03/app.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/portafolio')
+def portafolio():
+    return render_template('portafolio.html')
+
+
 app.run(debug=True)
