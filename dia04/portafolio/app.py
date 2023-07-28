62896f9e9ced6029ff3427869ff7df944ee171dc
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-27 22:30:01 -0500

    dia4 portafolio desde firebase

--- dia04/portafolio/app.py
+++ dia04/portafolio/app.py
@@ -1,31 +1,11 @@
 from flask import Flask,request,render_template
 from profile import Profile
+from firebase import FirebaseAdmin
 
 app = Flask(__name__)
 
-lista_proyectos = [
-    {
-        'codigo':'1',
-        'titulo':'Ecommerce',
-        'imagen':'https://peruretail.sfo3.cdn.digitaloceanspaces.com/wp-content/uploads/comparativa-de-las-plataformas-de-ecommerce-mas-populares-1.png',
-        'descripcion':'ecommerce creado con react.js',
-        'url':'https://www.linio.com.pe'
-    },
-    {
-        'codigo':'2',
-        'titulo':'Clon de Udemy',
-        'imagen':'https://i.ytimg.com/vi/YupUQMakNZ4/maxresdefault.jpg',
-        'descripcion':'clon de udemy creado con stack MERN',
-        'url':'https://www.udemy.com'
-    },
-    {
-        'codigo':'3',
-        'titulo':'Clon de Netflix',
-        'imagen':'https://dev-to-uploads.s3.amazonaws.com/uploads/articles/uf9hg224wv9ke1mz140r.png',
-        'descripcion':'clon de netflix creado con django',
-        'url':'https://www.netflix.com'
-    }  
-]
+fb = FirebaseAdmin()
+
 
 @app.route('/')
 def index():
@@ -41,6 +21,7 @@ def index():
 
 @app.route('/portafolio')
 def portafolio():
+    lista_proyectos = fb.get_collection('proyectos')
     context = {
         'proyectos':lista_proyectos
     }
