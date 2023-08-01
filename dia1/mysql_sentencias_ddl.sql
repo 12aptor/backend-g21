b0a8a3aba4991d0f898288d7fc1f93cb6bd5c80b
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-31 22:44:18 -0500

    dia1 ddl

--- dia1/mysql_sentencias_ddl.sql
+++ dia1/mysql_sentencias_ddl.sql
@@ -1,7 +1,55 @@
 show databases;
 
--- para crear base de datos
-create database colegio;
-
 -- para eliminar base de datos
-drop database colegio;
+drop database if exists colegio;
+
+-- para crear base de datos
+create database if not exists colegio;
+
+-- para seleccionar una base de datos
+use colegio;
+
+-- mostrar tablas
+show tables;
+
+-- para crear una tabla
+create table alumnos(
+	nombre varchar(255),
+	email varchar(100)
+);
+
+-- modificar una tabla para agregar un campo
+alter table alumnos
+add column biografia text;
+
+-- agregar un primary key
+alter table alumnos
+add column id int(11) not null primary key auto_increment first;
+
+-- modificar una columna
+alter table alumnos
+modify nombre varchar(255) not null;
+
+-- agregar una restriccion de valor unico
+alter table alumnos 
+add unique(email);
+
+-- para mostrar la estructura de una tabla
+describe alumnos;
+
+-- para eliminar una tabla
+drop table alumnos;
+
+-- creación de tabla desde el inicio
+-- colegio.alumnos definition
+
+CREATE TABLE `alumnos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `biografia` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+);
+
+
