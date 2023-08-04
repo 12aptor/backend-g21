d30d23297e4d735c61ad1bc272db553a815a5e2c
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-03 22:51:48 -0500

    dia4 catalogo get

--- dia4/app/__init__.py
+++ dia4/app/__init__.py
@@ -3,6 +3,10 @@ from .catalogo import catalogo
 from .cliente import cliente
 from .factura import factura
 
+from app.mysql_lib import MysqlDb
+mysql_db = MysqlDb('localhost','root','','db_factura_g21')
+db = mysql_db.get_conn()
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(catalogo)
--- dia4/app/catalogo/views.py
+++ dia4/app/catalogo/views.py
@@ -1,7 +1,22 @@
 from flask import Flask,render_template,request
 
 from . import catalogo
+from app import db
 
 @catalogo.route('/')
 def index():
-    return render_template('catalogo/index.html')
+    cursor = db.cursor(dictionary=True)
+    sql_productos = """select producto_id as id,
+                       producto_descripcion as descripcion,
+                       producto_precio as precio
+                       from tbl_producto
+                    """
+                    
+    cursor.execute(sql_productos)
+    data = cursor.fetchall()
+    
+    context = {
+        'productos':data
+    }
+                    
+    return render_template('catalogo/index.html',**context)
