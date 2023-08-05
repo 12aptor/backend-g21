a4acb540ccc329ecc3301b20bd45b628aed20a93
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-04 22:15:27 -0500

    dia5 insertar productos

--- dia5/app/catalogo/views.py
+++ dia5/app/catalogo/views.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 
 from . import catalogo
 from app import db
@@ -13,6 +13,7 @@ def index():
                     """
     cursor.execute(sql_productos)
     data = cursor.fetchall()
+    cursor.close()
     print(data)
     context = {
         'productos':data
@@ -22,11 +23,29 @@ def index():
 
 from .forms import CatalogoForm
 
-@catalogo.route('/nuevo')
+@catalogo.route('/nuevo',methods=['GET','POST'])
 def nuevo():
     catalogo_form = CatalogoForm()
+    titulo = "NUEVO PRODUCTO"
+    
+    if catalogo_form.validate_on_submit():
+        descripcion = catalogo_form.descripcion.data
+        precio = catalogo_form.precio.data
+        
+        cursor = db.cursor()
+        sql_insert_producto = """
+                              insert into tbl_producto(producto_descripcion,producto_precio)
+                              values('"""+ descripcion +"""',"""+ str(precio) +""")
+                              """
+        print(sql_insert_producto)
+        cursor.execute(sql_insert_producto)
+        db.commit()
+        cursor.close()
+        titulo = "PRODUCTO INSERTADO"
+        return redirect('/catalogo')
     
     context = {
+        'titulo':titulo,
         'form':catalogo_form
     }
     
