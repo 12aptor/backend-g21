042b4d9a81b5a32e066f30e370cd76faf386ab2e
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-04 21:55:26 -0500

    boostrap form

--- dia5/app/__init__.py
+++ dia5/app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
+from .config import Config
+
 from app.mysql_lib import MysqlDb
 mysql_db = MysqlDb('localhost','root','','db_factura_g21')
 db = mysql_db.get_conn()
@@ -9,6 +12,10 @@ from .factura import factura
 
 def create_app():
     app = Flask(__name__)
+    
+    bootstrap = Bootstrap5(app)
+    app.config.from_object(Config)
+    
     app.register_blueprint(catalogo)
     app.register_blueprint(cliente)
     app.register_blueprint(factura)
