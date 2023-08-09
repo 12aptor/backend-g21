f1130335b10da4cf1f879a0438d48b29e4584a22
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-08 22:48:59 -0500

    dia 2 cors en run

--- dia2/flask-api-todo/run.py
+++ dia2/flask-api-todo/run.py
@@ -1,8 +1,10 @@
 from flask import Flask,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.app_context().push()
 
