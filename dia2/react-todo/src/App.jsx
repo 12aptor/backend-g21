2992a7fc07f8d1871be8d32a11d33e2f0a04376a
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-09 20:51:18 -0500

    actualizar tarea

--- dia2/react-todo/src/App.jsx
+++ dia2/react-todo/src/App.jsx
@@ -11,11 +11,14 @@ class App extends React.Component{
       {
         tareas : [],
         descripcion:'',
-        estado:'pendiente'
+        estado:'pendiente',
+        id:0,
+        pos:null,
       }
     )
     this.cambioDescripcion = this.cambioDescripcion.bind(this)
     this.guardar = this.guardar.bind(this)
+    this.mostrar = this.mostrar.bind(this)
   }
 
   componentDidMount(){
@@ -35,23 +38,56 @@ class App extends React.Component{
     })
   }
 
+  mostrar(cod,index){
+    axios.get('http://localhost:5000/tarea/'+cod)
+    .then(res=>{
+      console.log(res.data.content)
+      this.setState({
+        descripcion:res.data.content.descripcion,
+        id:res.data.content.id,
+        pos:index
+      })
+    })
+  }
+
   guardar(e){
     e.preventDefault()
+    let cod = this.state.id
     const data = {
       descripcion : this.state.descripcion,
       estado : this.state.estado
     }
-    console.log(data)
-    axios.post('http://localhost:5000/tarea',data)
-    .then(res=>{
-      console.log(res.data.content)
-      this.state.tareas.push(res.data.content)
-      var temp = this.state.tareas
-      this.setState({
-        tareas :temp,
-        descripcion:''
+    console.log("enviando data al servidor :",data)
+    
+    if(cod>0){
+      //actualizar
+      axios.put('http://localhost:5000/tarea/'+cod,data)
+      .then(res=>{
+        let index_tarea = this.state.pos
+        this.state.tareas[index_tarea] = res.data.content
+        var temp = this.state.tareas
+        this.setState({
+          tareas : temp,
+          descripcion:'',
+          pos:null,
+          id:0
+        })
       })
-    })
+    }
+    else{
+      //insertar
+      axios.post('http://localhost:5000/tarea',data)
+      .then(res=>{
+        console.log("respuesta del servidor :",res.data.content)
+        this.state.tareas.push(res.data.content)
+        var temp = this.state.tareas
+        this.setState({
+          tareas :temp,
+          descripcion:''
+        })
+      })
+    }
+    
   }
 
   render(){
@@ -75,15 +111,21 @@ class App extends React.Component{
                 <th>Id</th>
                 <th>Descripcion</th>
                 <th>Estado</th>
+                <th>...</th>
               </tr>
             </thead>
             <tbody>
-              {this.state.tareas.map(tarea=>{
+              {this.state.tareas.map((tarea,index)=>{
                 return(
                   <tr key={tarea.id}>
                     <td>{tarea.id}</td>
                     <td>{tarea.descripcion}</td>
                     <td>{tarea.estado}</td>
+                    <td>
+                      <Button variant="success" onClick={()=>this.mostrar(tarea.id,index)}>
+                        Editar
+                      </Button>
+                    </td>
                   </tr>
                 )
               })}
