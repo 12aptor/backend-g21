ede19069f3f5ebe3892be385a607a238bcbf062e
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-14 21:59:20 -0500

    dia 1er blueprint listo

--- dia1/backend/app/__init__.py
+++ dia1/backend/app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from .config import Config
 
+from .api import api
+
 def create_app():
     app = Flask(__name__)
     CORS(app)
     
     app.config.from_object(Config)
+    app.register_blueprint(api)
     
     @app.route('/')
     def index():
--- dia1/backend/app/api/__init__.py
+++ dia1/backend/app/api/__init__.py
@@ -1,3 +1,5 @@
 from flask import Blueprint
 
 api = Blueprint('api',__name__,url_prefix='/api')
+
+from .models import Plato
