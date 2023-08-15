2a48d692f39fd1ff7168aa33ac22103ab32b8830
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-14 20:36:40 -0500

    dia1 database config

--- dia1/backend/app/__init__.py
+++ dia1/backend/app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-
-
+from .config import Config
 
 def create_app():
-    
     app = Flask(__name__)
     CORS(app)
+    
+    app.config.from_object(Config)
     
     @app.route('/')
     def index():
