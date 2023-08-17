ba628cd8abaf9811ba265bc74b9c47230cd541d8
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-16 19:51:28 -0500

    modelo categoria

--- dia2/backend/app/api/models.py
+++ dia2/backend/app/api/models.py
@@ -1,5 +1,24 @@
 from utils.db import db
 
+class Categoria(db.Model):
+    __tablename__ = "tbl_categoria"
+    
+    id = db.Column(db.Integer,primary_key=True)
+    nombre = db.Column(db.String(200),nullable=False)
+    imagen = db.Column(db.String(254),nullable=True)
+    
+    def __init__(self,nombre):
+        self.nombre = nombre
+        
+    @staticmethod
+    def get_all():
+        return Categoria.query.all()
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+
 class Plato(db.Model):
     __tablename__ = "tbl_plato"
     
