9023019d87c219daac0ea6685f3255864af7fe6e
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-17 20:44:08 -0500

    dia3 generando token

--- dia3/backend/app/__init__.py
+++ dia3/backend/app/__init__.py
@@ -4,6 +4,8 @@ from .config import Config
 
 from .api import api
 
+from flask_jwt_extended import create_access_token
+
 def create_app():
     app = Flask(__name__)
     CORS(app)
@@ -13,6 +15,10 @@ def create_app():
     
     @app.route('/')
     def index():
-        return '<center><h1>API REST TAIPAPP 1.0</h1></center>'
+        token = create_access_token(identity='1')
+        return """<center><h1>API REST TAIPAPP 1.0</h1>
+                </hr>
+                TOKEN : """+ token +"""
+                </center>"""
     
     return app
