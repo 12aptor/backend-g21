c1ce3addb01778dac28bf5a1a63ea70278746e25
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-17 21:30:38 -0500

    dia3 modelo usuario

--- dia3/backend/app/api/models.py
+++ dia3/backend/app/api/models.py
@@ -60,4 +60,30 @@ class Plato(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+        
+class Usuario(db.Model):
+    __tablename__ = "tbl_usuario"
+    
+    id = db.Column(db.Integer,primary_key=True)
+    nombre = db.Column(db.String(254),nullable=False)
+    email = db.Column(db.String(200),nullable=False,unique=True)
+    password = db.Column(db.Text,nullable=False)
+    isAdmin = db.Column(db.Boolean,default=False)
+    
+    @staticmethod
+    def get_all():
+        return Usuario.query.all()
+    
+    @staticmethod
+    def get_by_id(id):
+        return User.query.get(id)
+    
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
     
