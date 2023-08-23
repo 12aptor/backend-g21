621b367715840817da6fcd42bd37591108833953
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-22 22:40:06 -0500

    dia2 lista de peliculas

--- dia2/primerospasos/peliculas/web/views.py
+++ dia2/primerospasos/peliculas/web/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Pelicula
+
+def index(request):
+    lista_peliculas = Pelicula.objects.all()
+    print(lista_peliculas)
+    
+    context = {
+        'peliculas':lista_peliculas
+    }
+    
+    return render(request,'index.html',context)
