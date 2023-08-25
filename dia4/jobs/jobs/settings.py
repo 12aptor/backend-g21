f3ab1946faadac35de454418af0eba8635d12140
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-24 21:59:04 -0500

    dia4 database settings

--- dia4/jobs/jobs/settings.py
+++ dia4/jobs/jobs/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_jobs_g21',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- dia4/jobs/web/views.py
+++ dia4/jobs/web/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    titulo = 'PORTAL DE OFERTAS LABORALES'
+    context = {
+        'titulo':titulo
+    }
+    return render(request,'index.html',context)
