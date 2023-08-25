ae20768bf164348a4647ada1f24c4331ff4dca99
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-24 22:43:44 -0500

    admin site

--- dia4/jobs/web/admin.py
+++ dia4/jobs/web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    Categoria,Ubicacion,
+    Oferta
+)
+
+admin.site.register(Categoria)
+admin.site.register(Ubicacion)
+admin.site.register(Oferta)
