7ccede8382ef85195983419d2ec2d02d36b8d5b9
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-24 22:54:36 -0500

    index listo

--- dia4/jobs/web/views.py
+++ dia4/jobs/web/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
+from .models import (
+    Categoria,Ubicacion,
+    Oferta
+)
+
 # Create your views here.
 def index(request):
     titulo = 'PORTAL DE OFERTAS LABORALES'
+    lista_categorias = Categoria.objects.all()
+    lista_ubicaciones = Ubicacion.objects.all()
+    lista_ofertas = Oferta.objects.all()
+    
     context = {
-        'titulo':titulo
+        'titulo':titulo,
+        'categorias':lista_categorias,
+        'ubicaciones':lista_ubicaciones,
+        'ofertas':lista_ofertas
     }
     return render(request,'index.html',context)
