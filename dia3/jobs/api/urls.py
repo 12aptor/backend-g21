7fd447cf0a65953b3e8f8c4a310431702d743632
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-29 23:01:09 -0500

    dia3 get pu delete con generics views

--- dia3/jobs/api/urls.py
+++ dia3/jobs/api/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('categoria', views.CategoriaView.as_view()),
-    path('empresa',views.EmpresaView.as_view())
+    path('categoria/<int:categoria_id>',views.CategoriaDetailView.as_view()),
+    path('empresa',views.EmpresaView.as_view()),
+    path('empresa/<int:empresa_id>',views.EmpresaDetailView.as_view())
 ]
--- dia3/jobs/api/views.py
+++ dia3/jobs/api/views.py
@@ -13,6 +13,17 @@ class CategoriaView(generics.ListCreateAPIView):
     queryset = TblCategoria.objects.all()
     serializer_class = CategoriaSerializer
     
+class CategoriaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TblCategoria.objects.all()
+    lookup_url_kwarg = 'categoria_id'
+    serializer_class = CategoriaSerializer
+    
+    
 class EmpresaView(generics.ListCreateAPIView):
     queryset = TblEmpresa.objects.all()
+    serializer_class = EmpresaSerializer
+    
+class EmpresaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TblEmpresa.objects.all()
+    lookup_url_kwarg = 'empresa_id'
     serializer_class = EmpresaSerializer
