e673ea2a5670a8b704805b07449c83f074827ec7
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-29 19:55:01 -0500

    models y serializers

--- dia2/movimientos/api/models.py
+++ dia2/movimientos/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Movimiento(models.Model):
+    
+    TIPOS_CHOICES = (
+        ('1','INGRESO'),
+        ('2','EGRESO')
+    )
+    
+    tipo = models.CharField(max_length=1,choices=TIPOS_CHOICES)
+    fecha = models.DateField(auto_now_add=True)
+    concepto = models.CharField(max_length=200)
+    monto = models.DecimalField(max_digits=10,decimal_places=2)
+    saldo = models.DecimalField(max_digits=10,decimal_places=2)
+    
+    def __str__(self):
+        return self.concepto
+    
---
+++ dia2/movimientos/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Movimiento
+
+class MovimientoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movimiento
+        fields = '__all__'
