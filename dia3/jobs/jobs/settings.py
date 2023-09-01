a84d5f13e2ae7c842f8dfb2fde8172561db12ab4
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-31 20:13:19 -0500

    jwt

--- dia3/jobs/jobs/settings.py
+++ dia3/jobs/jobs/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'api',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- dia3/jobs/jobs/urls.py
+++ dia3/jobs/jobs/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView
+)
+
 urlpatterns = [
     path('api/',include('api.urls')),
     path('admin/', admin.site.urls),
+    path('api/login', TokenObtainPairView.as_view(), name='token_obtain_pair')
 ]
