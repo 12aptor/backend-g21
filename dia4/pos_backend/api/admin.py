94c516e2a7f484974dbcc0d1fc560f878ada954f
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-01 22:01:13 -0500

    dia4 panel listo

--- dia4/pos_backend/api/admin.py
+++ dia4/pos_backend/api/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
-from .models import Mesa,Categoria
+from .models import Mesa,Categoria,Plato
 
 admin.site.register(Mesa)
 admin.site.register(Categoria)
+
+@admin.register(Plato)
+class PlatoAdmin(admin.ModelAdmin):
+    
+    def imagen_html(self,obj):
+        return format_html('<img src="{}" width=150px/>'.format(obj.plato_img.url))
+    
+    imagen_html.short_description = 'Image'
+    
+    list_display = ('plato_nom','plato_pre','imagen_html')
+    list_filter = ('categoria_id',)
+    
+
+
