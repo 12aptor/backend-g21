c1a20446ccab75cfcd986d9e40f37c95ac322e99
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-05 21:27:15 -0500

    dia1 os memoria

--- dia1/os.js
+++ dia1/os.js
@@ -9,3 +9,9 @@ console.log('procesador : ' + procesador)
 console.log('sistema    : ' + sistema)
 console.log('cpu        : ' + cpu)
 console.log('memoria    : ' + memoria)
+/* implementar una promesa que permita mostrar
+la memoria ram en kb,mb y gb
+1024 bytes = 1 kb
+1024 kb = 1 mb
+1024 mb = 1 gb
+*/
