cb5ce5801843ca5f01ffe655d496169482946122
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-06 21:30:12 -0500

    dia2

--- dia2/proyecto_web/index.js
+++ dia2/proyecto_web/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',function(req,res){
-    res.send('<h1>Mi primer servidor con express</h1>')
+    res.send('<h1><center>Mi primer servidor con express</center></h1>')
 })
 
 app.listen(5000,()=>console.log('servidor en http://localhost:5000'))
