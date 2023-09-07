bcc60119985cac64581e361e525df46eb9fb4070
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-06 22:00:23 -0500

    dia2 post

--- dia2/proyecto_web/index.js
+++ dia2/proyecto_web/index.js
@@ -1,9 +1,37 @@
 const express = require('express')
+const bp = require('body-parser')
+
 
 const app = express()
+app.use(bp.urlencoded({extended:true}))
+
+app.use(express.static('public'))
 
 app.get('/',function(req,res){
     res.send('<h1><center>Mi primer servidor con express</center></h1>')
+})
+
+app.post('/calculadora',(req,res)=>{
+    let operacion = req.body.ope
+    let n1 = req.body.n1
+    let n2 = req.body.n2
+    let resultado = 0
+    switch(operacion){
+        case "suma":
+            resultado = parseInt(n1) + parseInt(n2)
+            res.send(" la suma es " + resultado)
+            break
+        case "resta":
+            resultado = parseInt(n1) - parseInt(n2)
+            res.send(" la resta es " + resultado)
+            break
+        case "multiplicación":
+            resultado = parseInt(n1) * parseInt(n2)
+            res.send(" la multiplicación es " + resultado)
+            break
+        default :
+            res.send(" no se encontro la operación solicitada")
+    }
 })
 
 app.get('/saludo',(req,res)=>{
