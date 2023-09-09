8cd99d7370682206caa246498c0ff52abeb956d1
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-08 19:58:08 -0500

    dia4 middlewares

--- dia4/ecommerce_backend/src/index.js
+++ dia4/ecommerce_backend/src/index.js
@@ -9,6 +9,17 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(function (req, res, next) {
+    const timeElapsed = Date.now()
+    const today = new Date(timeElapsed)
+    console.log('Time:', today.toISOString());
+    next();
+});
+
+app.use('/categories',(req,res,next)=>{
+    console.log('request url : ',req.originalUrl)
+    next()
+})
 
 app.get('/',(req,res)=>{
     try{
