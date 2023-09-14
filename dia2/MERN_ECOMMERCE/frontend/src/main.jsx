380d0b6bd2eeb200781bad3a6e006bcb9b4b3663
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-13 22:49:16 -0500

    plantilla en react

--- dia2/MERN_ECOMMERCE/frontend/src/main.jsx
+++ dia2/MERN_ECOMMERCE/frontend/src/main.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './css/bootstrap.css'
+import './css/chosen.css'
+import './css/elements-media.css'
+import './css/elements.css'
+import './css/font-awesome.min.css'
+import './css/media.css'
+import './css/style.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
