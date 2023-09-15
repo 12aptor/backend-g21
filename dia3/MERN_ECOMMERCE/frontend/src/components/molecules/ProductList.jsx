f1dfa1987c3631a578092c113e4e2dcf1bb60a76
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-14 21:46:19 -0500

    products

--- dia3/MERN_ECOMMERCE/frontend/src/components/molecules/ProductList.jsx
+++ dia3/MERN_ECOMMERCE/frontend/src/components/molecules/ProductList.jsx
@@ -1,39 +1,30 @@
+import {useEffect,useState} from 'react'
+import { getAllProducts } from '../../services/ProductoServices'
+import ProductCard from './ProductCard'
+
+
 const ProductList = ()=>{
+    const [listOfProducts,setListOfProducts] = useState([])
+
+    useEffect(()=>{
+      const fetchData = async()=>{
+        const response = await getAllProducts()
+        if(response.status === 200){
+          setListOfProducts(response.data)
+        }
+      }
+      fetchData()
+    },[])
+
+
     return(
         <div className="row prod-items prod-items-2">
-                        <article className="cf-sm-6 cf-md-6 cf-lg-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 sectgl-item">
-                          <div className="sectgl prod-i">
-                            <div className="prod-i-top">
-                              <a className="prod-i-img" href="product.html">
-                                <img src="http://placehold.it/290x258" alt=""/>
-                              </a>
-                              <div className="prod-i-actions">
-                                <div className="prod-i-actions-in">
-                                  <div className="prod-li-favorites">
-                                    <a href="wishlist.html" className="hover-label add_to_wishlist"><i className="icon ion-heart"></i><span>Add to Wishlist</span></a>
-                                  </div>
-                                  <p className="prod-quickview">
-                                    <a href="#" className="hover-label quick-view"><i className="icon ion-plus"></i><span>Quick View</span></a>
-                                  </p>
-                                  <p className="prod-i-cart">
-                                    <a href="#" className="hover-label prod-addbtn"><i className="icon ion-android-cart"></i><span>Add to Cart</span></a>
-                                  </p>
-                                  <p className="prod-li-compare">
-                                    <a href="compare.html" className="hover-label prod-li-compare-btn"><span>Compare</span><i className="icon ion-arrow-swap"></i></a>
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="prod-i-bot">
-                              <div className="prod-i-info">
-                                <p className="prod-i-price">$120.00</p>
-                                <p className="prod-i-categ"><a href="catalog-gallery.html">Lighting</a></p>
-                              </div>
-                              <h3 className="prod-i-ttl"><a href="product.html">Searchlight Portable</a></h3>
-                            </div>
-                          </div>
-                        </article>            
-                      </div>
+          {
+            listOfProducts.map((product)=>(
+              <ProductCard key={product.id} product={product}/>
+            ))
+          }
+        </div>
     )
 }
 
--- dia3/MERN_ECOMMERCE/frontend/src/services/CategoriesServices.js
+++ dia3/MERN_ECOMMERCE/frontend/src/services/CategoriesServices.js
@@ -1,11 +1,8 @@
 import {API_URL} from "../lib/Enviroments"
 
 export const getAllCategoriesService = async () =>{
-    const response = await fetch('http://127.0.0.1:5000/categories')
-    //console.log(response)
+    const response = await fetch(`${API_URL}/categories`)
     const status = response.status
     const data = await response.json()
-    console.log("status response : ",status)
-    //console.log(data)
     return {data,status}
 }
