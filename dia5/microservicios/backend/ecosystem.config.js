945ed76b2c623728991f2ee83dd04377846f300b
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-18 21:33:45 -0500

    dia5 pm2 ecosystem.config

---
+++ dia5/microservicios/backend/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name   : "ms-catalogo",
+    script : "./src/microservices/ms-catalogo/index.js"
+  },
+  {
+    name   : "ms-usuario",
+    script : "./src/microservices/ms-usuario/index.js"
+  }]
+}
