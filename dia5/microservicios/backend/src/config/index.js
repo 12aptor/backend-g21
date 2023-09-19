9542e9f6868946d1754572a4745652d2ea56e265
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-18 20:25:31 -0500

    dia5 ms catalogo

--- dia5/microservicios/backend/src/config/index.js
+++ dia5/microservicios/backend/src/config/index.js
@@ -8,6 +8,12 @@ const config = {
         cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
         api_key : process.env.CLOUDINARY_API_KEY,
         api_secret : process.env.CLOUDINARY_API_SECRET
+    },
+    mscatalogo:{
+        port:process.env.MSCATALOGO_PORT || '5001'
+    },
+    msusuario : {
+        port:process.env.MSUSUARIOS_PORT || '5002'
     }
 }
 
